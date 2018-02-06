84a13bd0dabcfd444b6392630410cd244e6f5387
Сергей Бобров <sbobrov85@gmail.com>
2018-02-06 00:14:31 +0000

    Fix init script

--- init-env.sh
+++ init-env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Init env on codeanywhere.com and ubuntu 14.04 with php 5 (replace to 7)
+# Init env on ubuntu 14.04 with php 5 (replace to 7)
 
 sudo apt-get update
 sudo apt-get dist-upgrade -y
@@ -7,5 +7,6 @@ sudo apt-get install software-properties-common
 sudo add-apt-repository -y ppa:ondrej/php
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 4F4EA0AAE5267A6C
 sudo apt-get remove -y php5
+sudo apt-get update
 sudo apt-get install -y php7.0 php7.0-mysql libapache2-mod-php7.0 php7.0-phalcon
 sudo apt-get autoremove -y
