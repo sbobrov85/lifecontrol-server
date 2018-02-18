d3049594ea1b59a669d3ad4bdc491095e5fe3f16
Dmitry <shds174@gmail.com>
2018-02-18 15:32:33 +0500

    Added response handler

--- public/index.php
+++ public/index.php
@@ -38,6 +38,8 @@ try {
 
         if (is_array($result)) {
            $app->response->setContent(json_encode($result));
+        } elseif (!empty($result) && !is_array($result)) {
+            $app->response->setContent($result);
         } elseif (empty($result)) {
             $app->response->setContent('204', 'No Content');
         } else {
